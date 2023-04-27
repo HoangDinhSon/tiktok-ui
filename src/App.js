d96ef7a50aa5c2e670ef5fda8f6b647b39d24e30
HoangDinhSon <dinhson040494@gmail.com>
2023-04-27 23:13:56 +0700

    bai 61

--- src/App.js
+++ src/App.js
@@ -1,153 +1,39 @@
-import { Button } from "react-bootstrap";
-import { Form } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
-import { Component } from "react";
-
-
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userName: "nhập tên đăng nhập",
-      userPassword: "",
-      userTextArea: "",
-      gentleList: 1,
-      rdLang:"vn",
-      checkBoxStatus:true
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+function App() {
+    function setupLayout(route) {
+        if (route.layout !== undefined) {
+            if (route.layout===null){return Fragment}
+            return route.layout;
+        } else {
+            return DefaultLayout;
+        }
     }
-    this.onHandleChange = this.onHandleChange.bind(this);
-    this.onHandleSubmit = this.onHandleSubmit.bind(this);
-
-  }
-
-  //lấy giá trị the3 input và update cho state 
-  onHandleChange(event) {
-    let target = event.target;
-    let name = target.name
-    let value = target.type==="checkbox"?target.checked : target.value;
-    this.setState({
-      [name]: value
-    })
-
-  }
-  // lấy cái state ra xử lí 
-  onHandleSubmit(event) {
-    event.preventDefault();
-    console.log(this.state);
-  }
-
-  render() {
     return (
-      <div className="container mt-30">
-        <div className="row">
-          <div className="col-lg-8 ">
-            <div className="panel panel-primary">
-              <div className="panel-heading">
-                <div className="panel-title">
-                  Form
-                </div>
-              </div>
-              <div className="panel-body">
-
-
-                <form onSubmit={this.onHandleSubmit}>
-                  <div className="form-group">
-                    <label>Username: </label>
-                    <input onChange={this.onHandleChange} type="text" placeholder="nhập giá trị vào " name="userName" /><br />
-                    <label>Password: </label>
-                    <input onChange={this.onHandleChange} type="password" placeholder="Nhập password " name="userPassword" />
-                  </div>
-                  <div className="form-group">
-                    <textarea name="userTextArea" onChange={this.onHandleChange}>
-
-                    </textarea>
-                  </div>
-                  <div>
-                    <label htmfor="gentleList">giới tính </label>
-                    <select
-                      value={this.state.gentleList}
-                      id="gentleList" name="gentleList"
-                      onChange={this.onHandleChange}
-                    >
-                      <option value={1} >Nam</option>
-                      <option value={0} >Nữ</option>
-                    </select>
-                  </div>
-                  <label>ngôn ngữ </label>
-                  <div className="radio" >
-                    <label>
-                      <input
-                        type="radio"
-                        name="rdLang"
-                        value="en"
-                        onChange={this.onHandleChange}
-                        checked ={this.state.rdLang==="en"}
-                      />
-                      Tiếng Anh
-                    </label><br />
-                    
-                    <label>
-                      <input
-                        type="radio"
-                        name="rdLang"
-                        value="vn"
-                        onChange={this.onHandleChange}
-                        checked ={this.state.rdLang==="vn"}
-                      />
-
-                      tiếng việt
-                    </label><br />
-                   
-                    <label>
-                      <input
-                        type="radio"
-                        name="rdLang"
-                        value="fran"
-                        onChange={this.onHandleChange}
-                        checked ={this.state.rdLang==="fran"}
-                      />
-
-                      tiếng pháp
-                    </label><br />
-                    <div className="checkbox">
-                    <label>
-                    <input 
-                    name="checkBoxStatus"
-                    value={true}
-                     type="checkbox"
-                      onChange={this.onHandleChange}
-                      checked={this.state.checkBoxStatus===true}
-                     />
-                    Trang thái
-                    </label><br/>
-                    <label>
-                    <input type="checkbox"/>
-                    son
-                    </label><br/>
-                    <label>
-                    <input type="checkbox"/>
-                    son
-                    </label><br/>
-
-                    </div>
-
-                  </div>
-                  <button type="submit" className="btn btn-primary">submit</button>&nbsp;
-                  <button type="reset" className="btn btn-default">xóa trắng </button>
-                </form>
-
-
-              </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map(function (route, index) {
+                        const Layout = setupLayout(route);
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
             </div>
-          </div>
-        </div>
-      </div>
-
+        </Router>
     );
-  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from '~/App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css'
-import "bootstrap/dist/js/bootstrap"
+import GlobalStyles from "./components/GlobalStyles"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <GlobalStyles>
     <App />
+  </GlobalStyles>
   </React.StrictMode>
 );
 
